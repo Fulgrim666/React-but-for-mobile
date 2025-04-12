95ea39aac79ede1a2d7a08eabc9ae1c9e3ea8b6d
Nikita Dmitriev <hazynik@mail.ru>
2025-04-12 20:49:08 +0300

    created user atom

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,12 @@
+import { useAtom } from "jotai";
 import { View, Text } from "react-native";
+import { profileAtom } from "../entities/user/model/user.state";
 
 export default function Courses () {
-    
+    const [profile] = useAtom(profileAtom)
     return <View>
         <Text style={{color: 'white'}}>
-            Курсы
+            {profile.profile?.name}
         </Text>
     </View>
 }
